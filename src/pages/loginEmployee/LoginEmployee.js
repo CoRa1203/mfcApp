20bc9acfe650bbdd80def1c4e4bf0e3322650a80
Aniloni <AnastasiaEfremova4003@mail.ru>
2024-09-22 20:17:45 +0400

    старый LoginEmployee

--- src/pages/loginEmployee/LoginEmployee.js
+++ src/pages/loginEmployee/LoginEmployee.js
@@ -1,11 +1,19 @@
-import HeaderMini from "../../components/header/HeaderMini";
-import Button from '../../components/buttons/Button';
+import Button from '../../components/buttons/Button'
 import "./../../generalStyles.scss";
-import Input from "../../components/input/input";
-import loginIcon from './../../imgMFC/loginIcon.svg'
-import "../loginEmployee/LoginEmployee.scss";
-
+import AccountEntry from "../../components/accountEntry/AccountEntry";
+import { useState } from "react";
+// const URL_SERVER = '' путь к серверу
 const LoginEmployee = function() {
+  const [loginParametr, setLoginParametr] = useState('')
+  const handleClickLogin = async() => {
+    try {
+      //логика отправки логина и пароля на сервер
+      // const response = axios.post(URL_SERVER, {loginParametr})
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  console.log(loginParametr)
   return (
     <div className="section"> 
     {/* className="section" применен flex для выравниван я по центру */}
@@ -13,15 +21,14 @@ const LoginEmployee = function() {
         <div className="container">
         {/* wrapper - ограничивающий контейнер 382px */}
         <HeaderMini/>
-        <div className="container-loginEmployee">
-            <img src={loginIcon} alt="loginIcon" />
-            <div className="text-loginEmployee">ВХОД В УЧЕТНУЮ ЗАПИСЬ</div>
-        </div>
-        <div className="input-loginEmployee"> <Input label={"ИНН / СНИЛС / ID"}></Input> </div>
-        <Button>ВОЙТИ</Button> 
-        </div>
+        <AccountEntry/>
+        <form>
+        <input value={loginParametr} onChange={(e) => setLoginParametr(e.target.value)}/>
+        <Button onClick={handleClickLogin}/>
+        </form>
         </div>
       </div>
+    </div>
   );
 }
 
